d0ae57b63aa27b9584da0cb40cc039b2dc0341e4
tschroeer72 <t.schroeer@web.de>
2024-08-26 10:32:23 +0200

    Komponenten für Infomodul

---
+++ Kursprojekt/UCComponents/PersonCompo.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WikkiDBLib.Models.ViewModels;
+
+namespace Kursprojekt.UCComponents
+{
+    /// <summary>
+    /// Interaktionslogik für PersonCompo.xaml
+    /// </summary>
+    public partial class PersonCompo : UserControl
+    {
+        public PersonCompo(PersonStadtVM personStadtVM)
+        {
+            InitializeComponent();
+            DataContext = personStadtVM;
+            ShowStatus(personStadtVM);
+        }
+
+        private void ShowStatus(PersonStadtVM personStadtVM)
+        {
+            lblStatus.Foreground = Brushes.DarkRed;
+            if (!personStadtVM.TestAbgeschlossen)
+            {
+                lblStatus.Content = "Keine Angabe...";
+            }
+            else
+            {
+                if (personStadtVM.Infiziert)
+                {
+                    lblStatus.Content = "Infiziert";
+                }
+                else
+                {
+                    lblStatus.Foreground = Brushes.ForestGreen;
+                    lblStatus.Content = "Nicht infiziert";
+                }
+            }
+        }
+    }
+}
